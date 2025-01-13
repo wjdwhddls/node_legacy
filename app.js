aeb27fea8a66ebaeb95da1bd147c0dcc7ea5b8d3
jjong_0425 <bfpark99@naver.com>
2025-01-13 15:25:00 +0900

    test : 각종 라우트 함수의 HTTP 메서드 기본 작동 테스트

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const app = express()
+const port = 3000
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.get('/',  (req, res) => {    //  => , function(req,res) 같은 표기법이다.
+  console.group('Got a GET request from Client')
+  res.send('Got a response from Server')
+})
+app.post('/',  (req, res) => {    
+  console.group('Got a POST request from Client')
+  res.send('Got a response from Server')
+})
+app.put('/user',  (req, res) => {   
+  console.group('Got a PUT request from Client')
+  res.send('Got a response from Server')
+})
+app.delete('/user',  (req, res) => {     //  실행할때 /user이라고 적혀있으면 이것도 같이 적어줘야한다.
+  console.group('Got a DELETE request from Client')
+  res.send('Got a response from Server')
 })
 
-app.listen(3000)
+app.listen(port, () => {
+  console.log(`Node Legacy App listening on port ${port}`)
+})
