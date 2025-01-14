74539a39c30b72027bf47dcbac3be6d1c154372e
jjong_0425 <bfpark99@naver.com>
2025-01-14 14:25:55 +0900

    chore : 데이터베이스 사용을 위한 MySQL2 설치

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
+const mysql = require('mysql2');
 const app = express();
 const port = 3000;
 
