4fda3756131b940c2c76c1d34ac19c4f81af2ba3
jjong_0425 <bfpark99@naver.com>
2025-01-14 11:16:29 +0900

    chore : 메인 어플리케이션이 바디파서를 사용하도록 설정

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
-const express = require('express')
-const ejs = require('ejs')
-const app = express()
-const port = 3000
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
 
+// View engine 설정
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
+// Static file serving
+app.use(express.static(__dirname + '/public'));
 
+// body-parser 미들웨어 설정
+app.use(bodyParser.urlencoded({ extended: false })); // URL-encoded 데이터
+app.use(bodyParser.json()); // JSON 데이터 파싱
+
+// GET 요청 라우터
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/blog', (req, res) => {
+  res.render('blog');
+});
+
+app.get('/users', (req, res) => {
+  res.render('users');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact'); // ./views/contact.ejs
+});
+
+// POST 요청 처리 라우터
 app.post('/api/contact', (req, res) => {
   const name = req.body.name;
   const phone = req.body.phone;
@@ -12,29 +40,10 @@ app.post('/api/contact', (req, res) => {
   const memo = req.body.memo;
 
   const data = `${name} ${phone} ${email} ${memo}`;
-  res.send(data);
+  res.send(data); // 클라이언트에 데이터 반환
 });
-app.set('view engine', 'ejs')
-app.set('views', './views')
 
-// static file serving
-app.use(express.static(__dirname+'/public'))
-
-app.get('/',  (req, res) => {    //  => , function(req,res) 같은 표기법이다.
-  res.render('index')
-})
-app.get('/blog',  (req, res) => {    //  => , function(req,res) 같은 표기법이다.
-  res.render('blog')
-})
-app.get('/users',  (req, res) => {    //  => , function(req,res) 같은 표기법이다.
-  res.render('users')
-
-})
-
-app.get('/contact', (req,res) => {
-  res.render('contact')
-})
-
+// 서버 시작
 app.listen(port, () => {
-  console.log(`Node Legacy App listening on port ${port}`)
-})
+  console.log(`Node Legacy App listening on port ${port}`);
+});
